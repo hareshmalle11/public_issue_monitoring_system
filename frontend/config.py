cdd2095fb14037a509ae5eabce9d3f4170d52914
hareshmalle11 <hareshmalle11@.com>
2026-06-05 10:17:28 +0530

    model moved to bakcend

--- frontend/config.py
+++ frontend/config.py
@@ -1,4 +1,6 @@
 import os
 
+import streamlit as st
+
 # API Backend server connection
-API_URL = "http://127.0.0.1:8000"
+API_URL = st.secrets.get("API_URL", os.getenv("API_URL", "http://127.0.0.1:8000"))
